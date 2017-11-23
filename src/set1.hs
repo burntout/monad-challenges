eaeb1ffe66efb53566b0d05cb4033a6e899f9380
Kate Dawson <k4t@3msg.es>
2017-11-23 22:25:07 +0000

    Completed Set 1 part 3 monad Challenge

--- src/set1.hs
+++ src/set1.hs
@@ -1,11 +1,40 @@
 import MCPrelude
 
+type Gen a = Seed -> (a, Seed)
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA f prf s  = (f $ fst $ prf s, snd $ prf s )
+
+rand' :: Gen Integer
+rand' = rand
+
 fiveRands :: [Integer]
-fiveRands = map fst $ take 5 $ iterate ( rand . snd ) $ rand $ mkSeed 1
-
+-- fiveRands = map fst $ take 5 $ iteraterand . snd ) $ rand $ mkSeed 1
+fiveRands = take 5 $ map fst $ iterate ( rand' . snd ) $ rand' $ mkSeed 1
+-- 
+randLetter :: Gen Char
+-- randLetter s = (toLetter $ fst $ rand' s, snd $ rand' s)
+randLetter = generalA toLetter rand'
+-- 
 randString3 :: String
-randString3 = map (toLetter . fst )  $ take 3 $ iterate ( rand . snd ) $ rand $ mkSeed 1
+-- randString3 = map (toLetter . fst ) $ take 3 $ iterate ( rand . snd ) $ rand $ mkSeed 1
+randString3 = take 3 $ map fst $ iterate ( randLetter . snd ) $ randLetter $ mkSeed 1
 
--- newtype Gen a Seed = Gen a Seed
-data Gen a = Gen a Seed deriving(Eq, Show)
-gen (a,s) = Gen a s
+randEven :: Gen Integer -- the output of rand * 2
+-- randEven s = ((*2) $ fst $ rand' s, snd $ rand' s)
+randEven = generalA (*2) rand'
+
+randOdd :: Gen Integer -- the output of rand * 2 + 1
+-- randOdd s = (((+1) . (*2)) $ fst $ rand' s, snd $ rand' s)
+-- randOdd s = ((+1)  $ fst $ randEven s, snd $ randEven s)
+randOdd = generalA (+1) randEven
+
+randTen :: Gen Integer -- the output of rand * 10
+-- randTen s = ((*10) $ fst $ rand' s, snd $ rand' s)
+randTen = generalA (*10) rand'
+
+
+
+
+
+
