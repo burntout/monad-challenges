6d60bda4f572c92714382557f51ec98530e2c71b
Kate Dawson <k4t@3msg.es>
2017-11-25 20:38:46 +0000

    generalizing lists of generators
    	modified:   set1.hs

--- src/set1.hs
+++ src/set1.hs
@@ -3,13 +3,13 @@ import MCPrelude
 type Gen a = Seed -> (a, Seed)
 
 generalA :: (a -> b) -> Gen a -> Gen b
-generalA f prf s  = (f $ fst $ prf s, snd $ prf s )
+generalA f prf s = (f $ fst $ prf s, snd $ prf s)
 
 rand' :: Gen Integer
 rand' = rand
 
 fiveRands :: [Integer]
--- fiveRands = map fst $ take 5 $ iteraterand . snd ) $ rand $ mkSeed 1
+-- fiveRands = map fst $ take 5 $ iterate ( rand . snd ) $ rand $ mkSeed 1
 fiveRands = take 5 $ map fst $ iterate ( rand' . snd ) $ rand' $ mkSeed 1
 -- 
 randLetter :: Gen Char
@@ -33,8 +33,29 @@ randTen :: Gen Integer -- the output of rand * 10
 -- randTen s = ((*10) $ fst $ rand' s, snd $ rand' s)
 randTen = generalA (*10) rand'
 
+randPair :: Gen (Char, Integer)
+-- randPair s = ((chr, int ),s'')
+--     where 
+--         (chr,s') = randLetter s 
+--         (int, s'') = rand' s'
+randPair = generalPair randLetter rand'
 
+generalPair :: Gen a -> Gen b -> Gen (a,b)
+-- generalPair x y s = ((a, b), s'')
+--     where
+--         (a, s') = x s 
+--         (b, s'') = y s'
 
+generalPair = generalB (,)
 
+generalB :: (a -> b -> c ) -> Gen a -> Gen b -> Gen c
+generalB f x y s = (f a b, s'')
+    where 
+        (a, s') = x s
+        (b, s'') = y s'
 
-
+repRandom :: [Gen a] -> Gen [a]
+-- repRandom [] = \s -> ([],s)
+repRandom [] = \s -> ([], s)
+repRandom (x:xs) = generalB (:) x $ repRandom xs
+   
