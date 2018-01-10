a814d117b72241e977d61b94806d9a2683051d34
Kate Dawson <k4t@3msg.es>
2018-01-10 22:46:50 +0000

    Got some help with these from someone who knows stuff!

--- src/Set2.hs
+++ src/Set2.hs
@@ -68,6 +68,54 @@ link :: Maybe a -> (a -> Maybe b) -> Maybe b
 link Nothing f = Nothing
 link (Just y) f = f y
             
-
 queryGreek2 gd key =  link (link (link xs tailMay) maximumMay) (\m -> (link (link xs headMay) (\h -> divMay (fromIntegral m) (fromIntegral h))))
         where xs = lookupMay key gd
+
+addSalaries :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries salaryData p1 p2 = link s1 (\x -> (link s2 (\y -> Just (x + y))))
+    where
+        s1 = lookupMay p1 salaryData
+        s2 = lookupMay p2 salaryData
+
+
+--ylink :: Maybe a-> Maybe b -> (a -> b -> Maybe c) -> Maybe c
+--ylink m n f = link m (\x -> (link n (\y -> f x y)))
+
+yLink :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+-- yLink f _ Nothing = Nothing
+-- yLink f Nothing _ = Nothing
+-- yLink f (Just x) (Just y) = Just (f x y)
+yLink f a b = link a (\x -> (link b (\y -> Just (f x y))))
+
+addSalaries2 :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries2 salaryData p1 p2 = yLink (+) s1 s2
+    where
+        s1 = lookupMay p1 salaryData
+        s2 = lookupMay p2 salaryData
+
+mkMaybe :: a -> Maybe a
+mkMaybe = Just
+
+tailProd :: Num a => [a] -> Maybe a
+tailProd xs = link (tailMay xs) (Just . product) 
+
+tailSum :: Num a => [a] -> Maybe a
+tailSum xs = link (tailMay xs) (Just . sum) 
+
+--transMaybe :: a -> ( a -> Maybe b) -> ( b -> c) -> Maybe c
+--transMaybe a f g = link (f a) (Just . g)  
+
+transMaybe :: (a -> b) -> Maybe a -> Maybe b
+transMaybe f m = link m (Just . f)
+
+tailProd' :: Num a => [a] -> Maybe a
+tailProd' xs = transMaybe product $ tailMay xs
+
+tailSum' :: Num a => [a] -> Maybe a
+tailSum' xs = transMaybe sum $ tailMay xs
+
+tailMax :: Ord a => [a] -> Maybe (Maybe a)
+tailMax xs = transMaybe maximumMay $ tailMay xs
+
+combine :: Maybe (Maybe a) -> Maybe a
+combine m = link m id
